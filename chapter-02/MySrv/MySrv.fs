48dbc0327634809c8bc43baef0d436273ac4f895
Henrik Feldt <henrik@haf.se>
2017-01-01 17:21:00 +0100

    Reorganising 02 -> 03

--- chapter-02/MySrv/MySrv.fs
+++
@@ -1,5 +0,0 @@
-open Suave                 // always open suave
-open Suave.Successful      // for OK-result
-open Suave.Web             // for config
-
-startWebServer defaultConfig (OK "Hello World!")
